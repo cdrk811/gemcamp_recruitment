47cd2fe571a80bc69a2f5e72948b32b28f1942a7
tabs424 <tabacugjasper@gmail.com>
2023-04-10 14:39:45 +0800

    add status filter in homepage and adjust applicant create

--- app/models/applicant_batch_ship.rb
+++ app/models/applicant_batch_ship.rb
@@ -13,9 +13,10 @@ class ApplicantBatchShip < ApplicationRecord
   has_one :gem_camp, class_name: 'Phase::GemCamp'
 
   accepts_nested_attributes_for :interview
+  accepts_nested_attributes_for :applicant
 
-  scope :show_to_home, -> { joins(:batch).where('batches.id = ?', Batch.open_status&.id)
-                                         .joins(:interview).where('interviews.status = ?', :pending) }
+  scope :current_opened_batch, -> { joins(:batch).where('batches.id = ?', Batch.open_status&.id) }
+  scope :filter_by_interview_status, ->(status) { joins(:interview).where('interviews.status IN (?)', status) }
 
   before_create :set_application_date, if: Proc.new { application_date.blank? }
   after_create :generate_phase_interview!
